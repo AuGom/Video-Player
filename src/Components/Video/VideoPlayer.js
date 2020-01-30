9e608983da23ded948cf9f51dc4f5901289e616c
AuGom <augusto.pg@live.com>
2020-01-30 19:15:21 -0300

    Play and pause added

--- src/Components/Video/VideoPlayer.js
+++ src/Components/Video/VideoPlayer.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const VideoPlayer = () => {
+  const video = useRef();
+
+  const playAndPause = () => {
+    video.current.paused ? video.current.play() : video.current.pause();
+  };
+
   return (
-    <video src="http://sveltejs.github.io/assets/caminandes-llamigos.mp4" />
+    <video
+      ref={video}
+      onClick={playAndPause}
+      src="http://sveltejs.github.io/assets/caminandes-llamigos.mp4"
+    />
   );
 };
 
